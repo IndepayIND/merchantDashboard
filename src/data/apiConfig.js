ea2d610617a52cfae9464bcb31249e7368aa0157
manish.goyal@indepay.com <manish.goyal@indepay.com>
2023-07-18 15:48:56 +0530

    config issue

--- src/data/apiConfig.js
+++ src/data/apiConfig.js
@@ -1,7 +1,7 @@
 let baseUrl;
 
 if (window.location.host.includes("localhost") || window.location.host.includes("dev")) {
-    baseUrl = "https://alpha.tara.app";
+    baseUrl = "https://dev.tara.app";
 } else if (window.location.host.includes("uat")) {
     baseUrl = "https://alpha.tara.app";
 } else {
